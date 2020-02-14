a48c3b956593edb776e2fe1eec9d8f84510abb78
ShireenPierre <ttpolitricks@gmail.com>
2020-02-13 20:03:18 -0400

    Lab 3 Final

--- question2.js
+++ question2.js
@@ -7,7 +7,7 @@ day = prompt("Please enter a day:");
 
 month_30days = month == 4 || month == 6 || month == 9 || month == 11;
 
-if(day <= 0 || day > 31 || month <= 0 || month > 12 || month == 2 && day > 29 || month == 2 && day >= 29 && !(year%4 == 0) || month_30days == day > 30)
+if(day <= 0 || day > 31 || month <= 0 || month > 12 || month == 2 && day > 29 || month == 2 && day >= 29 && !(year%4 == 0) || month_30days && day > 30)
 {
     alert("Invalid date");
 }
